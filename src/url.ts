1991fbcd79c07ae0c1b8224650b3bf10d20bd5af
ibuckner <ibuckner@users.noreply.github.com>
2020-07-31 10:13:13 +0100

    v0.5.5

--- src/url.ts
+++ src/url.ts
@@ -1,12 +1,14 @@
-/**
- * Tests for URLs prefixed with one of [file, ftp, http, https]
- */
-const reURL = /\b(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?:\w+-?)*\w+)(?:\.(?:\w+-?)*\w+)*(?:\.(?:\w{2,}))(?::\d{2,5})?(?:\/[^\s]*)?/gmi;
+import { find } from "./find";
+
+const reURL: RegExp[] = [
+  /\b(?:(?:https?|file|ftps?):\/\/)(?:\S+(?::\S*)?@)?(?:(?:\w+-?)*\w+)(?:\.(?:\w+-?)*\w+)*(?:\.(?:\w{2,}))(?::\d{2,5})?(?:\/[^\s]*)?/gmi,
+  /\b[a-z\.]+\/\w+\b/gmi
+];
 
 /**
  * Returns iterator of urls located in string
  * @param d - string to test
  */
 export function findURL(d: string): RegExpMatchArray[] {
-  return [...d.matchAll(reURL)];
+  return find(d, reURL);
 }
