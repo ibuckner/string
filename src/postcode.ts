750c3ae8098b2addfd63714257a2662e2a7c2525
ibuckner <ibuckner@users.noreply.github.com>
2020-08-10 13:42:05 +0100

    v0.6.8

--- src/postcode.ts
+++ src/postcode.ts
@@ -1,7 +1,17 @@
 /**
  * Tests for UK postcode
  */
-const rePostcode = /\b(([A-Z]{1,2}[0-9][A-Z0-9]?|0[LX]\d{1,2}|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA)\s?[0-9][A-Z]{2}|BFPO\s?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[\s-]?[0-9]{4}|GE\s?CX|GIR\s?0A{2}|SAN\s?TA1)\b/gi;
+const AA9A: string = `[A-Z][A-Z]\\d[ABEHMNPRVWXY]`;
+const A9A: string = `[A-Z]\\d[ABCDEFGHJKPSTUW]`;
+const A9: string = `[A-Z]\\d`;
+const A99: string = `[A-Z]\\d\\d`;
+const AA9: string = `[A-Z][A-Z]\\d`;
+const AA99: string = `[A-Z][A-Z]\\d\\d`;
+const ERR: string = `0[LX]\\d{1,2}`;  // handles problems where user has entered zero instead of 'O'
+const SP1: string = `BFPO\\s?\\d{1,4}|(KY\\d|MSR|VG|AI)[\\s-]?\\d{4}|GE\\s?CX|GIR\\s?0AA|SAN\\s?TA1`;
+const SP2: string = `ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA`;
+const re: string = `(?:(?:${AA9A}|${A9A}|${A9}|${A99}|${AA9}|${AA99}|${ERR}|${SP2})\\s?\\d[A-Z][A-Z]|${SP1})`;
+const rePostcode = new RegExp(`\\b${re}\\b`, "gi");
 
 /**
  * Returns iterator of UK postcodes located in string
