2416ce08965f2b813a6288d1a042111b614377b3
ibuckner <ibuckner@work>
2020-12-03 11:51:56 +0000

    Stricter URL interpretation

--- src/url.test.ts
+++ src/url.test.ts
@@ -1,8 +1,9 @@
 import { findURL } from "./url";
 
 test("findURL()", () => {
-  const m = findURL("Locating https://www.google.com or http://google.co.uk:8080, not.a.url./dhs33 and pic.twitter.com/C74RABu8pzE");
+  const m = findURL(`Locating https://www.google.com or http://google.co.uk:8080, 
+  not.a.url./dhs33 and pic.twitter.com/C74RABu8pzE`);
   expect(m[0][0]).toBe("https://www.google.com");
   expect(m[1][0]).toBe("http://google.co.uk:8080");
-  expect(m[2][0]).toBe("pic.twitter.com/C74RABu8pzE");
+  expect(m.length).toBe(2);
 });
